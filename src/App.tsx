72fe31a2863d4aa72f392b9dbdaf2e034616d425
Kevin Lan <kevinlan321@gmail.com>
2020-12-27 18:34:24 -0800

    setup components/pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import "./index.css"
+import "tailwindcss/dist/base.css"
+import Hero from "./pages/Hero"
+import Experience from "./pages/Experience"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <Hero />
+        <Experience />
+      </div>
+    )
+  }
 }
 
 export default App;
