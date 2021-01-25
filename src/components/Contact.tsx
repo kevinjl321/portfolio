c254bdb95a43e10cbfe5a58dbeee4284987cdff0
Kevin Lan <kevinlan321@gmail.com>
2021-01-25 09:41:15 -0800

    rev button color

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -72,7 +72,7 @@ export class Contact extends Component {
                     <div className="flex justify-center">
                         <button
                             type="submit"
-                            className="special px-12 py-2 bg-gray-900 mt-6 text-gray-300 font-bold text-center border-2 border-gray-200 transition duration-500 ease-in-out hover:bg-purple-300 hover:text-gray-900 hover:border-gray-900 text-xl lowercase rounded-full cursor-pointer font-header md:text-1xl"
+                            className="special px-12 py-2 bg mt-6 text-gray-300 font-bold text-center border-2 border-gray-200 transition duration-500 ease-in-out hover:bg-purple-300 hover:text-gray-900 hover:border-gray-900 text-xl lowercase rounded-full cursor-pointer font-header md:text-1xl"
                             aria-label="Send an email"
                         >
                             send
