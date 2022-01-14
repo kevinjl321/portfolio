ab19e2f7a48cc0c2732e3d384769af61768ea31f
Kevin Lan <kevinlan321@gmail.com>
2022-01-13 17:19:36 -0800

    condense descriptions

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -43,8 +43,8 @@ export class Projects extends Component {
                                     </a>
                                 </h4>
                                 <p className="block mb-6 dark:text-white">
-                                    Researched and architected a convolutional neural network to accurately detect COVID-19 from chest X-Rays, provided promising alternative to less efficient RT-PCR test. Worked with 
-                                    graduate students and co-authored a research paper, which was presented at a formal capstone seminar and published in artificial intelligence journal Annals of Biomedical Engineering.
+                                    Architected convolutional neural network capable of accurately detecting COVID-19 from chest X-Rays. Worked under 
+                                    PhD student Ryan Solgi, co-authored research paper and presented at formal capstone seminar. Published findings in scientific journal Annals of Biomedical Engineering.
                                 </p>
                                 <div className="flex flex-wrap">
                                     <span className="px-2 py-1 mb-2 text-gray-300 bg-gray-600 dark:bg-gray-800 rounded dark:text-gray-400">
