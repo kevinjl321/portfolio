6882382c27b7f7452ff65e55d6b88bfc2fcdcbc8
Kevin Lan <kevinjl321@berkeley.edu>
2023-09-01 12:31:19 -0700

    update description:

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,13 +13,13 @@ export class About extends Component {
           </div>
           <div className="mb-8">
             <p className="mb-8 text-xl md:text-2xl">
-              I'm currently a Computer Science and Data Science student at UC Berkeley passionate in exploring 
+              I'm currently a Computer Science and Economics student at UC Berkeley passionate in exploring 
               the intersections of society and technology. I'm interested in all things SWE and am actively seeking opportunities to expand my skillset. Sub-fields I've explored and/or plan on exploring include
               ML/AI, Computer Vision, Networks, Cloud Computing, Databases, Infrastructure, and Cybersecurity.
               <br></br>
               <br></br>
               Some of my hobbies include playing the latest PC titles, making random projects, binging dramas,
-              and going on long bike trips with friends. I'm also a nationally ranked badminton player!
+              and going on long bike trips with friends. I'm also a competitive badminton player!
             </p>
           </div>
         </div>
